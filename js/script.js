aa2ce72419f7b97a9087809bddaacf68c3e714c4
Haley <osullivanhaley25@gmail.com>
2023-05-02 23:13:47 -0500

    Adding js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log ("Hi! Welcome to my Portfolio Website.")
+
+function menuToggle()  {
+    var x = document.getElementById('myNavtoggle');
+}
+
+function menuToggle()  {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+}
+
+function menuToggle()  {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle')  {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
